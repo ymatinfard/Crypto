823d49c1d3fb8439171420dc3e7d76cd4fc7c619
Yousef <matinfard.y@gmail.com>
2024-07-17 17:23:12 +0200

    Add repository implementation

--- app/src/main/java/com/matin/youtech/crypto/data/Repository.kt
+++ app/src/main/java/com/matin/youtech/crypto/data/Repository.kt
@@ -2,14 +2,23 @@ package com.matin.youtech.crypto.data
 
 import com.matin.youtech.crypto.data.remote.RemoteDataSource
 import com.matin.youtech.crypto.domain.MarketListItem
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
 interface Repository {
     fun getMarketList(): Flow<List<MarketListItem>>
 }
 
-class RepositoryImpl(private val remoteDataSource: RemoteDataSource) : Repository {
-    override fun getMarketList(): Flow<List<MarketListItem>> {
-        TODO("Not yet implemented")
+class RepositoryImpl @Inject constructor(private val remoteDataSource: RemoteDataSource) :
+    Repository {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun getMarketList(): Flow<List<MarketListItem>> =
+        remoteDataSource.getMarketList().flatMapLatest {
+            flow {
+                emit(it.toDomain())
+            }
     }
 }
--- app/src/main/java/com/matin/youtech/crypto/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/matin/youtech/crypto/data/remote/RemoteDataSource.kt
@@ -1,24 +1,23 @@
 package com.matin.youtech.crypto.data.remote
 
-import com.matin.youtech.crypto.data.fakeMarketList
+import com.matin.youtech.crypto.data.getFakeMarketList
 import com.matin.youtech.crypto.data.model.MarketListItemEntity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 interface RemoteDataSource {
     fun getMarketList(): Flow<List<MarketListItemEntity>>
 }
 
-class RemoteDataSourceImpl(private val scope: CoroutineScope) : RemoteDataSource {
+class RemoteDataSourceImpl @Inject constructor(private val scope: CoroutineScope) :
+    RemoteDataSource {
     override fun getMarketList(): Flow<List<MarketListItemEntity>> = flow {
-        scope.launch {
-            while (true) {
-                delay(1000)
-                emit(fakeMarketList)
-            }
+        while (true) {
+            emit(getFakeMarketList())
+            delay(1000)
         }
     }
 }
