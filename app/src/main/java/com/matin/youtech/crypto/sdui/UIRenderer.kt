5c1550661491a2eed741159ae22a18d8b09c85b1
Yousef <matinfard.y@gmail.com>
2024-08-26 19:54:46 +0200

    refactor: Add horizontal padding for views

--- app/src/main/java/com/matin/youtech/crypto/sdui/UIRenderer.kt
+++ app/src/main/java/com/matin/youtech/crypto/sdui/UIRenderer.kt
@@ -1,7 +1,11 @@
 package com.matin.youtech.crypto.sdui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.matin.youtech.crypto.designsystem.Banner
 import com.matin.youtech.crypto.designsystem.TradeRow
 import com.matin.youtech.crypto.domain.model.Banner
@@ -12,7 +16,9 @@ import com.matin.youtech.crypto.domain.model.TradeRow
 class UIRenderer(val screen: Screen) {
     @Composable
     fun render() {
-        Column {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 6.dp)) {
             screen.components.forEach { component ->
                 when (component) {
                     is Banner -> Banner(component)
