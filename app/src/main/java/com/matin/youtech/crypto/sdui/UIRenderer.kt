083086636d7fcbd7a94848a172696261b292592c
Yousef <matinfard.y@gmail.com>
2024-08-28 18:29:44 +0200

    fix: Handle screen scrolling

--- app/src/main/java/com/matin/youtech/crypto/sdui/UIRenderer.kt
+++ app/src/main/java/com/matin/youtech/crypto/sdui/UIRenderer.kt
@@ -1,8 +1,8 @@
 package com.matin.youtech.crypto.sdui
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -19,13 +19,15 @@ import com.matin.youtech.crypto.domain.model.TradeRow
 object UIRenderer {
     @Composable
     fun render(screen: Screen) {
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = 6.dp)
         ) {
-            screen.components.forEach { component ->
-                componentRenderers[component::class.java]?.invoke(component)
+            item {
+                screen.components.forEach { component ->
+                    componentRenderers[component::class.java]?.invoke(component)
+                }
             }
         }
     }
